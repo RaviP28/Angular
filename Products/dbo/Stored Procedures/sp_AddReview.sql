a6dd6f8cd55ee08567c69a49f87c1122e10b1687
dpm-mayfield <dmayfi0325@sbcglobal.net>
2016-10-10 10:48:35 -0500

    Added database

---
+++ Products/dbo/Stored Procedures/sp_AddReview.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE sp_AddReview
+(
+	@productId int,
+	@stars int,
+	@author nvarchar(500),
+	@body ntext
+)
+AS
+INSERT INTO Review(
+	ProductId,
+	starts,
+	author,
+	body
+	)
+
+VALUES(
+	@productId,
+	@stars,
+	@author,
+	@body
+	)
---
+++ Products/dbo/Stored Procedures/sp_GetImagesForProduct.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE sp_GetImagesForProduct
+(
+	@productId int
+)
+AS
+
+SELECT
+	[full],
+	thumb,
+	id
+FROM
+	[Image]
+WHERE
+	ProductID = @productId
